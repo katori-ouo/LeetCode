c0e18082a586f3200f163705544666e282622958
Katori <fjj927@outlook.com>
2023-03-28 19:58:41 +0800

    贪心算法练习题

---
+++ LeetCode1005.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    static bool cmp(int a, int b)
+    {
+        return abs(a) > abs(b);
+    }
+
+    int largestSumAfterKNegations(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end(), cmp);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] < 0 && k > 0)
+            {
+                nums[i] *= -1;
+                k--;
+            }
+        }
+        if (k % 2 == 1)
+            nums[nums.size() - 1] *= -1;
+        int res = 0;
+        for (int num : nums)
+            res += num;
+        return res;
+    }
+};
---
+++ LeetCode45.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int jump(vector<int> &nums)
+    {
+        if (nums.size() == 1)
+            return 0;
+        int curDistance = 0;
+        int ans = 0;
+        int nextDistance = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            nextDistance = max(nextDistance, nums[i] + i);
+            if (i == curDistance)
+            {
+                if (curDistance < nums.size() - 1)
+                {
+                    ans++;
+                    curDistance = nextDistance;
+                    if (nextDistance >= nums.size() - 1)
+                        break;
+                }
+                else
+                    break;
+            }
+        }
+        return ans;
+    }
+};
