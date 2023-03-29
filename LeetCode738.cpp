04480a8e0edc82f0e18c4345384885a525729161
Katori <fjj927@outlook.com>
2023-03-29 16:03:30 +0800

    贪心算法练习题

---
+++ LeetCode738.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    int monotoneIncreasingDigits(int n)
+    {
+        string num = to_string(n);
+        int idx = num.size();
+        for (int i = num.size() - 1; i > 0; i--)
+        {
+            if (num[i - 1] > num[i])
+            {
+                idx = i;
+                num[i - 1]--;
+            }
+        }
+        for (int i = idx; i < num.size(); i++)
+        {
+            num[i] = '9';
+        }
+        return stoi(num);
+    }
+};
---
+++ LeetCode968.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int res;
+    int traversal(TreeNode *cur)
+    {
+        if (nur == nullptr)
+            return 2;
+        int left = traversal(cur->left);
+        int right = traversal(cur->right);
+
+        if (left == 2 && right == 2)
+            return 0;
+        if (left == 0 || right == 0)
+        {
+            res++;
+            return 1;
+        }
+        if (left == 1 || right == 1)
+        {
+            return 2;
+        }
+        return -1;
+    }
+    int minCameraCover(TreeNode *root)
+    {
+        res = 0;
+        if (traversal(root) == 0)
+            res++;
+        return res;
+    }
+};
