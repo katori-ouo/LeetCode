8f7781ed1f12b6408226f2397b0655b0711c2fb6
Katori <fjj927@outlook.com>
2023-03-29 23:24:47 +0800

    动态规划练习题

--- LeetCode96.cpp
+++ LeetCode96.cpp
@@ -5,15 +5,15 @@ using namespace std;
 class Solution
 {
 public:
-    int integerBreak(int n)
+    int numTrees(int n)
     {
         vector<int> dp(n + 1);
-        dp[2] = 1;
-        for (int i = 3; i <= n; i++)
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j <= i; j++)
             {
-                dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
+                dp[i] += dp[j - 1] * dp[i - j];
             }
         }
         return dp[n];
